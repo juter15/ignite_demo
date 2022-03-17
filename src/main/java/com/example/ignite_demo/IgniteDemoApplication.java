33bc74018ec5f26843a95e360a55bb2fb54e1e6f
Chan Ho <juter15@naver.com>
2022-03-17 17:33:51 +0900

    Initial commit

---
+++ src/main/java/com/example/ignite_demo/IgniteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ignite_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgniteDemoApplication.class, args);
+    }
+
+}
